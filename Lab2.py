a849ae0350291557b3e5bf28993f8c025512e41f
hong xiang <hongxiang17.23@ichat.sp.edu.sg>
2024-05-06 09:43:21 +0800

    changed Lab2

--- Lab2.py
+++ Lab2.py
@@ -1,39 +1,32 @@
+import statistics
 
 def display_main_menu():
     print("Welcome to the Temperature Calculator")
 
 def get_user_input():
-    num_list = input("Enter temp values separated by commas: ")
+    x = input("Enter temp values separated by commas: ")
+    num_list = x.split(", ")
+    num_list = [float(x) for x in num_list]
     return num_list
 
 def calc_average(num_list):
     # Calculate average temperature
-    numbers = [float(num) for num in num_list.split(",")]
-    average = sum(numbers) / len(numbers)
+    average = sum(num_list) / len(num_list)
     return average
 
 def calc_median_temperature(num_list):
-    # Calculate the median temperature in the list
-    numbers = sorted([float(num) for num in num_list.split(",")])
-    length = len(numbers)
-    if length % 2 == 0:
-        #median is division of 2 middle numbers
-        median = (numbers[length//2 - 1] + numbers[length//2]) / 2
-    else:
-        median = numbers[length//2]
-        #median is only 1 middle number
+    median = statistics.median(num_list)
     return median
 
 def sort_temperature(num_list):
     # Calculate sorted temperature in the list
-    sort_list = sorted([float(num) for num in num_list.split(",")])
+    sort_list = sorted(num_list)
     return sort_list
 
 def find_min_max(num_list):
     # Find minimum and maximum temperature in the list
-    numbers = [float(num) for num in num_list.split(",")]
-    min_temp = min(numbers)
-    max_temp = max(numbers)
+    min_temp = min(num_list)
+    max_temp = max(num_list)
     return min_temp, max_temp
 
 def main():
